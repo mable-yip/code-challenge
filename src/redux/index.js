0b6ee9cdd958417e5a01789edaecd0cbb8b4c245
mable-yip <47068733+mable-yip@users.noreply.github.com>
2021-03-23 13:12:46 +1100

    Modify code to use redux toolkit
    - createReducer and createAction

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,52 +1,18 @@
 import {createStore} from "redux"
+import { createAction, createReducer, current } from '@reduxjs/toolkit'
 
-export function addTodo(todo){
-    console.log(todo)
-    return({
-        type: "ADD_TODO",
-        payload: todo
-    })
-}
 
-export function deleteTodo(todo){
-    return({
-        type: "DELETE_TODO",
-        payload: todo
-    })
-}
-
-export function deleteAllTodos(){
-    console.log("deleted All")
-    return({
-        type: "DELETE_ALL_TODOS"
-    })
-}
-
-export function getAllTodos(){
-    console.log("reducer function GET_ALL_TODOS")
-    return({
-        type: "GET_ALL_TODOS"
-    })
-}
+export const addTodo = createAction('add_todo')
+export const deleteTodo = createAction('delete_todo')
+export const deleteAllTodos = createAction('delete_all_todo')
 
 const initialTodoList =[]
 
-function reducer(state=initialTodoList, action){
-    switch(action.type){ 
-        case "ADD_TODO":
-            console.log(state)
-            return [... state, action.payload]
-        case "DELETE_TODO":
-            return state.filter(todo => todo !== action.payload)
-        case "DELETE_ALL_TODOS":
-            return []
-        case "GET_ALL_TODOS":
-            console.log("reducer GET_ALL_TODOS", state)
-            return state
-        default:
-            return state
-    }
-}
+const reducer = createReducer(initialTodoList, {
+    [addTodo]: (state, action) => [... state, action.payload],
+    [deleteTodo]: (state, action) => current(state).filter(todo => todo !== action.payload),
+    [deleteAllTodos]: () => []
+})
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 store.subscribe(() => console.log(store.getState()))
